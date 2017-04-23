86742dcff9fe93ab76a6cef439dfc03f44c79add
xorphitus <xorphitus@gmail.com>
2017-04-24 02:33:19 +0900

    pythonを呼べるところまで

--- src/clojure_python_interaction/core.clj
+++ src/clojure_python_interaction/core.clj
@@ -1,6 +1,11 @@
-(ns clojure-python-interaction.core)
+(ns clojure-python-interaction.core
+  (:import (java.util Properties)
+           (org.python.util PythonInterpreter)))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn main []
+  (let [props (new Properties)]
+    (do
+      (.put props "python.console.encoding" "UTF-8")
+      (PythonInterpreter/initialize (System/getProperties) props (make-array String 0))
+      (let [interp (new PythonInterpreter)]
+        (.exec interp "print(1234)")))))
