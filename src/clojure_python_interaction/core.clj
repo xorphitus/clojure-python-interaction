0c4eb574e0f9b2a56c6c33f5b031fb8dc9a3ec09
xorphitus <xorphitus@gmail.com>
2017-04-25 00:12:11 +0900

    lein runできない等あったので修正

--- src/clojure_python_interaction/core.clj
+++ src/clojure_python_interaction/core.clj
@@ -2,10 +2,13 @@
   (:import (java.util Properties)
            (org.python.util PythonInterpreter)))
 
-(defn main []
+(defn -main []
   (let [props (new Properties)]
     (do
       (.put props "python.console.encoding" "UTF-8")
       (PythonInterpreter/initialize (System/getProperties) props (make-array String 0))
       (let [interp (new PythonInterpreter)]
-        (.exec interp "print(1234)")))))
+        (.exec interp "import csv")
+        (.exec interp "from sklearn.naive_bayes import MultinomialNB")
+        (.exec interp "from sklearn.feature_extraction.text import CountVectorizer")
+        (.exec interp "vectorizer = CountVectorizer(stop_words=[])")))))
